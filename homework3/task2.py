4222b826fcd923a6511834a2586cbf65c80b5396
Altyn Latypova <altynlatypova@gmail.com>
2024-10-16 07:46:23 +0300

    Add files via upload

---
+++ homework3/task2.py
@@ -0,0 +1,20 @@
+def dec_tri(dec): #Преобразование  десятичного числа в двоичную систему счисления
+ if dec == 0:
+  return "0"
+ elif dec > 0:
+  return dec_tri(dec // 3) + str(dec % 3)
+ else:
+  return "-" + dec_tri(-dec)
+def dec_input(): #Ввод числа пользователем
+ try:
+  dec_number = float(input("Введите десятичное число: "))
+  if dec_number != int(dec_number):
+   print("Введенное число - вещественное, оно преобразовано в целое.")
+  return int(dec_number)
+ except ValueError:
+      print("Ошибка! Введите целое число.")
+      return dec_input()
+
+dec_number = dec_input()
+tri_number = dec_tri(dec_number)
+print(f"Троичное представление:", tri_number)
